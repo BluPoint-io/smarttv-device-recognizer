fec8d5d4212b67c26dfa5a9cf730a1abe54c9a8b
Hakkı Can İlkbahar <hilkbahar@blutv.com>
2019-03-18 21:58:55 +0300

    setCurrentDevice methods are refactored

--- src/index.js
+++ src/index.js
@@ -1,22 +1,8 @@
 import platforms from './statics/platforms';
 
-
 class Recognizer {
 	static setCurrentDevice() {
-		const userAgent = navigator.userAgent;
-		let currentDevice = {
-			agentIndex: 'browserDefault',
-			brandName: 'web',
-			modelYear: '2017',
-			displayName: 'Default Browser'
-		};
-		for (let platform = 0; platform < platforms.length; platform += 1) {
-			if (userAgent.indexOf(platforms[platform].agentIndex) >= 0) {
-				currentDevice = platforms[platform];
-				break;
-			}
-		}
-		return currentDevice;
+		return platforms.find(x => navigator.userAgent.indexOf(x.agentIndex) > -1) || platforms[0];
 	}
 
 	static getKeyCodes(brandName) {
