695f843c605a93186dbd9c22562029c86be7eb93
İrfan Simsar <isimsar@blutv.com>
2019-08-26 14:21:01 +0300

    Update userAgent index list to handle details

--- src/statics/platforms.js
+++ src/statics/platforms.js
@@ -13,157 +13,199 @@ const platforms = [
 		agentIndex: 'Maple 5',
 		brandName: 'samsung',
 		modelYear: '2010',
-		displayName: 'Samsung SmartTV Orsay | 2010'
+		displayName: 'Samsung Orsay | 2010'
 	},
 	{
 		agentIndex: 'Maple 6',
 		brandName: 'samsung',
 		modelYear: '2011',
-		displayName: 'Samsung SmartTV Orsay | 2011'
+		displayName: 'Samsung Orsay | 2011'
 	},
 	{
 		agentIndex: 'SmartTV; Maple2012',
 		brandName: 'samsung',
 		modelYear: '2012',
-		displayName: 'Samsung SmartTV Orsay | 2012'
+		displayName: 'Samsung Orsay | 2012'
 	},
 	{
 		agentIndex: 'SmartTV+2013; Maple2012',
 		brandName: 'samsung',
 		modelYear: '2013',
-		displayName: 'Samsung SmartTV Orsay | 2013'
+		displayName: 'Samsung Orsay | 2013'
 	},
 	{
 		agentIndex: 'SmartTV+2014; Maple2012',
 		brandName: 'samsung',
 		modelYear: '2014',
-		displayName: 'Samsung SmartTV Orsay | 2014'
+		displayName: 'Samsung Orsay | 2014'
 	},
 	{
 		agentIndex: 'SmartTV+2015; Maple2012',
 		brandName: 'samsung',
 		modelYear: '2015',
-		displayName: 'Samsung SmartTV Orsay | 2015'
+		displayName: 'Samsung Orsay | 2015'
 	},
 	{
 		agentIndex: 'Maple',
 		brandName: 'samsung',
 		modelYear: 'undefined',
-		displayName: 'Samsung SmartTV Orsay'
+		displayName: 'Samsung Orsay'
 	},
 	{
 		agentIndex: 'Tizen 2.3',
 		brandName: 'tizen',
 		modelYear: '2015',
-		displayName: 'Samsung SmartTV Tizen | 2015'
+		displayName: 'Samsung Tizen | 2015'
 	},
 	{
 		agentIndex: 'Tizen 2.4.0',
 		brandName: 'tizen',
 		modelYear: '2016',
-		displayName: 'Samsung SmartTV Tizen | 2016'
+		displayName: 'Samsung Tizen | 2016'
 	},
 	{
 		agentIndex: 'Tizen 4.0',
 		brandName: 'tizen',
 		modelYear: '2017',
-		displayName: 'Samsung SmartTV Tizen | 2017'
+		displayName: 'Samsung Tizen | 2017'
 	},
 	{
 		agentIndex: 'Tizen',
 		brandName: 'tizen',
 		modelYear: '2015',
-		displayName: 'Samsung SmartTV Tizen | 2015'
+		displayName: 'Samsung Tizen | 2015'
+	},
+	{
+		agentIndex: 'MB110',
+		brandName: 'vestel',
+		modelYear: 'undefined',
+		displayName: 'Vestel MB110'
+	},
+	{
+		agentIndex: 'MB120',
+		brandName: 'vestel',
+		modelYear: 'undefined',
+		displayName: 'Vestel MB120'
+	},
+	{
+		agentIndex: 'MB130',
+		brandName: 'vestel',
+		modelYear: 'undefined',
+		displayName: 'Vestel MB130'
+	},
+	{
+		agentIndex: 'MB211',
+		brandName: 'vestel',
+		modelYear: 'undefined',
+		displayName: 'Vestel MB211'
 	},
 	{
 		agentIndex: 'VESTEL',
 		brandName: 'vestel',
 		modelYear: 'undefined',
-		displayName: 'Vestel SmartTV'
+		displayName: 'Vestel'
 	},
 	{
 		agentIndex: 'VSTVB',
 		brandName: 'vestel',
 		modelYear: 'undefined',
-		displayName: 'Vestel SmartTV'
+		displayName: 'Vestel'
 	},
 	{
 		agentIndex: 'LG NetCast.TV-2011',
 		brandName: 'lg',
 		modelYear: '2011',
-		displayNme: 'LG SmartTV NetCast | 2011'
+		displayNme: 'LG NetCast | 2011'
 	},
 	{
 		agentIndex: 'LG NetCast.TV-2012',
 		brandName: 'lg',
 		modelYear: '2012',
-		displayName: 'LG SmartTV NetCast | 2012'
+		displayName: 'LG NetCast | 2012'
 	},
 	{
 		agentIndex: 'LG NetCast.TV',
 		brandName: 'lg',
 		modelYear: '2013',
-		displayName: 'LG SmartTV NetCast | 2013'
+		displayName: 'LG NetCast | 2013'
 	},
 	{
 		agentIndex: 'LG SimpleSmart.TV-2016',
 		brandName: 'lg',
 		modelYear: '2016',
-		displayName: 'LG SmartTV NetCast | 2016'
+		displayName: 'LG NetCast | 2016'
 	},
 	{
 		agentIndex: 'NETTV/3',
 		brandName: 'philips',
 		modelYear: '2011',
-		displayName: 'Philips SmartTV | 2011'
+		displayName: 'Philips | 2011'
 	},
 	{
 		agentIndex: 'NETTV/4.0',
 		brandName: 'philips',
 		modelYear: '2012',
-		displayName: 'Philips SmartTV | 2012'
+		displayName: 'Philips | 2012'
 	},
 	{
 		agentIndex: 'NETTV/',
 		brandName: 'philips',
 		modelYear: '2013',
-		displayName: 'Philips SmartTV | 2013'
+		displayName: 'Philips | 2013'
 	},
 	{
 		agentIndex: 'DuneHD/',
 		brandName: 'philips',
 		modelYear: 'undefined',
-		displayName: 'Philips SmartTV'
+		displayName: 'Philips'
 	},
 	{
 		agentIndex: 'Viera/1.',
 		brandName: 'viera',
 		modelYear: '2012',
-		displayName: 'Viera SmartTV | 2012'
+		displayName: 'Viera | 2012'
 	},
 	{
 		agentIndex: 'Viera/3.',
 		brandName: 'viera',
 		modelYear: '2013',
-		displayName: 'Viera SmartTV | 2013'
+		displayName: 'Viera | 2013'
 	},
 	{
 		agentIndex: 'Web0S',
 		brandName: 'webos',
 		modelYear: 'undefined',
-		displayName: 'LG WebOS SmartTV'
+		displayName: 'LG WebOS'
+	},
+	{
+		agentIndex: 'Grundig',
+		brandName: 'arcelik',
+		modelYear: 'undefined',
+		displayName: 'Grundig - Arçelik'
+	},
+	{
+		agentIndex: 'Beko',
+		brandName: 'arcelik',
+		modelYear: 'undefined',
+		displayName: 'Beko - Arçelik'
+	},
+	{
+		agentIndex: 'Altus',
+		brandName: 'arcelik',
+		modelYear: 'undefined',
+		displayName: 'Altus - Arçelik'
 	},
 	{
 		agentIndex: 'Arcelik',
 		brandName: 'arcelik',
 		modelYear: 'undefined',
-		displayName: 'Arçelik Group Smart TV'
+		displayName: 'Arçelik'
 	},
 	{
 		agentIndex: 'Hisense',
 		brandName: 'hisense',
 		modelYear: 'undefined',
-		displayName: 'Hisense SmartTV'
+		displayName: 'Hisense'
 	},
 	{
 		agentIndex: 'SRAF',
@@ -175,7 +217,7 @@ const platforms = [
 		agentIndex: 'NETRANGEMMH',
 		brandName: 'web',
 		modelYear: 'undefined',
-		displayName: 'Netrange Group Smart TV'
+		displayName: 'Netrange Group'
 	}
 ];
 
